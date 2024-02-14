cb61583624acbc25075ba0e924f3d7e2f0c1c660
AriyatamsWebsites <158802326+AriyatamsWebsites@users.noreply.github.com>
2024-02-14 17:22:16 +0700

    Create AlaphAndPlutonium.js

---
+++ mods/AlaphAndPlutonium.js
@@ -0,0 +1,25 @@
+// Made by TheOriginalFlyingDuckling
+
+
+
+// Plutonium
+
+elements.plutonium = {
+    color: "#446f90",
+    behavior: behaviors.POWDER,
+    category: "solids",
+    state: "solid",
+    reactions: {},
+}
+
+
+// Alaph
+elements.alaph = {
+    behavior: behaviors.POWDER,
+    category: "solids",
+    state: "solid",
+    color: ["#446f45","#000000","#0000ff"],
+    breakInto: "plutonium",
+    breakIntoColor: ["#FF0000","#800080"],
+    reactions: {},
+}
